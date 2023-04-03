eb72258a62969c2e48b227ad0da0d94401d64f6f
HUAN <terikihuan@gmail.com>
2023-04-03 16:58:11 +0700

    change app version to v5

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 
 // This is a branch called 11-13, and I just updated it
 // Let's see if my pull request trigger the deployment
+// This should be a version 5 app
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('3') // change this string to ensure a new version deployed
+  res.send('5') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
